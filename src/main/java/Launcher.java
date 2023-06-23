29bc5d6fa19c31376453cd38b6d56d56609279c4
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-06-23 22:32:31 +0530

    launcher added

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+}
