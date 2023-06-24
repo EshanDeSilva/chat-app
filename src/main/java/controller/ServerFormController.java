2dd6a9e527cc93210ca2440e867bdc1dddcf4eeb
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-06-24 21:19:21 +0530

    emoji button implemented

--- src/main/java/controller/ServerFormController.java
+++ src/main/java/controller/ServerFormController.java
@@ -1,12 +1,16 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import com.vdurmont.emoji.EmojiParser;
+import emoji.EmojiPicker;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -21,6 +25,8 @@ public class ServerFormController {
     public VBox vBox;
     public JFXTextField txtMsg;
     public ScrollPane scrollPain;
+    public JFXButton emojiButton;
+    public AnchorPane pane;
 
     private Server server;
 
@@ -36,6 +42,8 @@ public class ServerFormController {
 //
 //        }).start();
 
+        txtMsg.setStyle("-fx-font-size: 14");
+
         vBox.heightProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
@@ -43,22 +51,62 @@ public class ServerFormController {
             }
         });
 
-        server.receiveMessageFromClient(vBox);
+//        server.receiveMessageFromClient(vBox);
+
+        sendMsg("Sever Starting..");
+        emoji();
+    }
+
+    private void emoji() {
+        // Create the EmojiPicker
+        EmojiPicker emojiPicker = new EmojiPicker();
+
+        VBox vBox = new VBox(emojiPicker);
+        vBox.setPrefSize(150,300);
+        vBox.setLayoutX(400);
+        vBox.setLayoutY(175);
+        vBox.setStyle("-fx-font-size: 30");
+
+        pane.getChildren().add(vBox);
+
+        // Set the emoji picker as hidden initially
+        emojiPicker.setVisible(false);
+
+        // Show the emoji picker when the button is clicked
+        emojiButton.setOnAction(event -> {
+            if (emojiPicker.isVisible()){
+                emojiPicker.setVisible(false);
+            }else {
+                emojiPicker.setVisible(true);
+            }
+        });
+
+        // Set the selected emoji from the picker to the text field
+        emojiPicker.getEmojiListView().setOnMouseClicked(event -> {
+            String selectedEmoji = emojiPicker.getEmojiListView().getSelectionModel().getSelectedItem();
+            if (selectedEmoji != null) {
+                txtMsg.setText(txtMsg.getText()+selectedEmoji);
+            }
+            emojiPicker.setVisible(false);
+        });
     }
 
     public void sendButtonOnAction(ActionEvent actionEvent) {
-        String msgToSend = txtMsg.getText();
+        sendMsg(txtMsg.getText());
+    }
+
+    private void sendMsg(String msgToSend) {
         if (!msgToSend.isEmpty()){
             HBox hBox = new HBox();
             hBox.setAlignment(Pos.CENTER_RIGHT);
             hBox.setPadding(new Insets(5,5,0,10));
 
             Text text = new Text(msgToSend);
-            text.setStyle("-fx-font-size: 13");
+            text.setStyle("-fx-font-size: 14");
             TextFlow textFlow = new TextFlow(text);
 
 //            #0693e3 #37d67a #40bf75
-            textFlow.setStyle("-fx-background-color: #0693e3; -fx-color: white; -fx-background-radius: 20px");
+            textFlow.setStyle("-fx-background-color: #0693e3; -fx-font-weight: bold; -fx-color: white; -fx-background-radius: 20px");
             textFlow.setPadding(new Insets(5,10,5,10));
             text.setFill(Color.color(1,1,1));
 
---
+++ src/main/java/emoji/EmojiPicker.java
@@ -0,0 +1,47 @@
+package emoji;
+
+import com.vdurmont.emoji.EmojiParser;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmojiPicker extends VBox {
+    private ListView<String> emojiListView;
+
+    public EmojiPicker() {
+        // Load the emoji images
+        List<String> emojis = new ArrayList<>();
+
+        String[] emojiHtmlList = new String[]{"&#128514;","&#10084;","&#128525;","&#129315;","&#128522;",
+                "&#128591;","&#128149;","&#128557;","&#128293;","&#128536;","&#128077;","&#129392;","&#128526;","&#128518;",
+                "&#128513;","&#128521;","&#129300;","&#128517;","&#128532;","&#128580;","&#128540;","&#9829;","&#9851;","&#128530;",
+        "&#128553;","&#9786;","&#128513;","&#128076;","&#128079;","&#128148;","&#128150;","&#128153;"};
+
+        for (String em:emojiHtmlList) {
+            emojis.add(EmojiParser.parseToUnicode(em));
+        }
+
+        // Create the emoji list view
+        emojiListView = new ListView<>();
+        emojiListView.setItems(FXCollections.observableArrayList(emojis));
+
+        // Customize the appearance of the list view
+//        emojiListView.setCellFactory(param -> new EmojiCell());
+
+        // Create the emoji picker hBox
+        HBox hBox = new HBox(emojiListView);
+        hBox.setPadding(new Insets(10));
+
+        // Set the picker hBox to the EmojiPicker
+        getChildren().add(hBox);
+    }
+
+    public ListView<String> getEmojiListView() {
+        return emojiListView;
+    }
+}
